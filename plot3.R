d08fea6442b381af5d69f7075ad78c65d72b67ca
ju0gri <ioana.grigoropol@gmail.com>
2017-01-03 21:20:27 +0200

    Update graphs with legend solid strips

--- plot3.R
+++ plot3.R
@@ -22,6 +22,6 @@ plot(feb_household_data$StrTime, feb_household_data$Sub_metering_1, type="l", co
 lines(feb_household_data$StrTime, feb_household_data$Sub_metering_2, col="red")
 lines(feb_household_data$StrTime, feb_household_data$Sub_metering_3, col="blue")
 
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty="solid")
 
 dev.off()
--- plot4.R
+++ plot4.R
@@ -32,7 +32,7 @@ plot(feb_household_data$StrTime, feb_household_data$Sub_metering_1, type="l", co
 lines(feb_household_data$StrTime, feb_household_data$Sub_metering_2, col="red")
 lines(feb_household_data$StrTime, feb_household_data$Sub_metering_3, col="blue")
 
-legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"))
+legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black", "red", "blue"), lty="solid")
 
 # reactive power plot
 plot(feb_household_data$StrTime, feb_household_data$Global_reactive_power, type="l", col="black", xlab="datetime",ylab="Global_reactive_power")
